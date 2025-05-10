87f65729a399844f0fb70135d4d3eb1db7a72e53
Bruno-Gomes-QA <bruno.gomes.qa@gmail.com>
2025-05-10 08:27:41 -0300

    aula

---
+++ Operadores/battle.py
@@ -0,0 +1,11 @@
+# Crie um sistema de comparação de dois personagens:
+
+# O programa deve:
+# Armazenar em variáveis os atributos de cada personagem;
+# Nome, Força, agilidade, inteligência
+# Determinar quem vence com base na comparação entre os atributos
+
+nome_pers_1 = input('Digite o nome do personagem 1: ')
+forca_pers_1 = input('\nSua força [1-20]: ')
+agilidade_pers_1 = input('Sua agilidade [1-20]: ')
+inteligencia_pers_1 = input('Sua inteligência [1-20]: ')
