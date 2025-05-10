5fa696a0c4d2bd44febcaa5072328133022dcda9
Bruno-Gomes-QA <bruno.gomes.qa@gmail.com>
2025-05-10 14:21:12 -0300

    Aula-10-05

--- Aula-10-05/battle.py
+++ Aula-10-05/battle.py
@@ -5,7 +5,13 @@
 # Nome, Força, agilidade, inteligência
 # Determinar quem vence com base na comparação entre os atributos
 
+
 nome_pers_1 = input('Digite o nome do personagem 1: ')
 forca_pers_1 = input('\nSua força [1-20]: ')
 agilidade_pers_1 = input('Sua agilidade [1-20]: ')
 inteligencia_pers_1 = input('Sua inteligência [1-20]: ')
+
+nome_pers_2 = input('Digite o nome do personagem 2: ')
+forca_pers_2 = input('\nSua força [1-20]: ')
+agilidade_pers_2 = input('Sua agilidade [1-20]: ')
+inteligencia_pers_2 = input('Sua inteligência [1-20]: ')
